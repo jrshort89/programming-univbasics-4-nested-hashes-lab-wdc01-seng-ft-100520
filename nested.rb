e85257469ffcb16cf926edd19e479099dd5c6f67
Jacob Short <jrshort89@gmail.com>
2020-09-01 00:56:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,6 +88,8 @@ def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
 
+  alans_new_info = "GUI"
+
 	programmer_hash = 
  		{
       :grace_hopper => {
