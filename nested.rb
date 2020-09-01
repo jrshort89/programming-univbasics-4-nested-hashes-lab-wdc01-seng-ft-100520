b1cf8a144f476ab2ad80db23fca6a38925186ca0
Jacob Short <jrshort89@gmail.com>
2020-09-01 00:51:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,13 +15,11 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:alan_kay][:known_for]
-	
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -37,7 +35,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,7 +54,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -66,8 +64,7 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-	programmer_hash[:yukihiro_matsumoto][:languages].unshift("LISP")
+	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
 	programmer_hash = 
  		{
@@ -84,7 +81,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash
 end
 
 def changing_alan
@@ -106,7 +103,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash
 end
 
 def adding_to_dennis
@@ -128,5 +126,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+  programmer_hash[:dennis_ritchie][:languages].push()
 
 end
